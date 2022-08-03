c115516a3d4132473633ca96823d1947fe8c84fe
Nataliya Taranina <ngtaranina@hotmail.com>
2022-08-03 14:03:19 +0300

    Solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+
+ArrayLength = bracketsConfig.length;
+  for (let i = 0; i < str.length; i++) {
+      let currentElement = str[i];
+      topElement = stack[stack.length - 1]; 
+      identificator = false;
+
+          for (let k = 0; k < bracketsConfig.length; k++) {           
+              if (currentElement === bracketsConfig[k][1]){           
+                      identificator = true
+                      if (currentElement === bracketsConfig[k][0]){  
+                            if (topElement === bracketsConfig[k][0]) {
+                                stack.pop(); 
+                                break;
+                                } 
+                                else {
+                                    stack.push(currentElement);
+                                    break;
+                                }
+
+                      } else {
+                        if (stack.length === 0) {
+                          return (false); 
+                        } 
+                            else{ 
+                                if (topElement === bracketsConfig[k][0]) {
+                                    stack.pop(); 
+                                    break;
+                                    } 
+                                    else {
+                                        stack.push(currentElement);
+                                        break;
+                                    }
+                            }
+                    }
+              } 
+        }                                   
+      if(!identificator) stack.push(currentElement); 
+  }
+return(stack.length === 0);
+} 
